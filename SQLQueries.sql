a0a93c636857838e2b6c498cdd9bee5230c8d02d
Fidelis <yonlifidelis2@gmail.com>
2023-07-20 22:40:01 +0200

    JJ

--- SQLQueries.sql
+++ SQLQueries.sql
@@ -2,51 +2,47 @@ CREATE DATABASE GitHub_Database;
 
 USE GitHub_Database;
 
-
------------- Langage de Définition (LDD)----
-
-
 -- Création de la table Projet
 CREATE TABLE Projet (
-    project_id INT PRIMARY KEY,
+    project_id INT NOT NULL IDENTITY PRIMARY KEY,
     nom NVARCHAR(255),
     full_name NVARCHAR(255),
     url_link NVARCHAR(500),
     descriptif NVARCHAR(MAX),
     stars INT,
     created_at DATETIME,
-    language_id INT,  -- Clé étrangère vers l'entité Langage
+    language_id INT REFERENCES Langage(language_id),  -- Foreign key referencing the 'Langage' entity
     forks INT,
     updated_at DATETIME,
-    license_id INT,  -- Clé étrangère vers l'entité Licence
-    type_of_owner NVARCHAR(50)
+    license_id INT REFERENCES Licence(license_id),  -- Foreign key referencing the 'Licence' entity
+    type_of_owner NVARCHAR(50),
 );
+
 
 -- Création de la table Langage
 CREATE TABLE Langage (
-    language_id INT PRIMARY KEY,
+    language_id INT IDENTITY PRIMARY KEY,
     language_name NVARCHAR(100)
 );
 
 -- Table Licence
 CREATE TABLE Licence (
-    license_id INT PRIMARY KEY,
+    license_id INT IDENTITY PRIMARY KEY,
     license_name NVARCHAR(100) NULL -- Autoriser les valeurs NULL
 );
 
 -- Table Contributeur
 CREATE TABLE Contributeur (
-    contributor_id INT PRIMARY KEY,
+    contributor_id INT IDENTITY PRIMARY KEY,
     contributor_name NVARCHAR(100) NULL -- Autoriser les valeurs NULL
 );
 
-
 -- Création de la table Sujet
 CREATE TABLE Sujet (
-    topic_id INT PRIMARY KEY,
+    topic_id INT IDENTITY PRIMARY KEY,
     topic_name NVARCHAR(100)
 );
-----------------------------------------------------------------------------------------------------------
+
 -- Création de la table Participation
 CREATE TABLE Participation (
     project_id INT,
@@ -64,5 +60,3 @@ CREATE TABLE Projet_Sujet (
     FOREIGN KEY (project_id) REFERENCES Projet(project_id),
     FOREIGN KEY (topic_id) REFERENCES Sujet(topic_id)
 );
-
-
